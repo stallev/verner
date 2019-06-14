3b42391bf36f6e273b45e21ea621d4aceaab2909
Alevander <stwork87@gmail.com>
2019-06-14 13:50:24 +0300

    converted for gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -16,24 +16,25 @@ var del = require('del');
 var rename = require('gulp-rename');
 var ghPages = require('gulp-gh-pages');
 
-gulp.task('style', function() {
-  gulp.src('sass/style.scss')
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer({browsers: [
-        'last 2 versions'
-      ]})
-    ]))
-    .pipe(cssComb())
-    .pipe(gulp.dest('build/css'))
-    .pipe(csso())
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('build/css'))
-    .pipe(server.stream());
+gulp.task('style', function(){ // Создаем таск Sass
+    return
+      gulp.src('sass/style.scss')
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(postcss([
+        autoprefixer({browsers: [
+          'last 2 versions'
+          ]})
+        ]))
+      .pipe(cssComb())
+      .pipe(gulp.dest('build/css'))
+      .pipe(csso())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('build/css'))
+      .pipe(server.stream()); // Обновляем CSS на странице при изменении
 });
 
-gulp.task('serve', ['style'], function() {
+gulp.task('serve', function() {
   server.init({
     server: 'build',
     notify: false,
@@ -41,43 +42,7 @@ gulp.task('serve', ['style'], function() {
     cors: true,
     ui: false
   });
-
-  gulp.watch('sass/**/*.{scss,sass}', ['style']);
-  gulp.watch('*.html', ['copyHtml']);
-  gulp.watch('js/*.js', ['copyJS']);
-  gulp.watch('build/*.html').on('change', server.reload);
-  gulp.watch('build/js/*.js').on('change', server.reload);
 });
-
-gulp.task('copy', function(){
-  return gulp.src([
-    'fonts/**/*.{eot,svg,ttf,woff,woff2}',
-    'js/**',
-    'img/*.svg',
-    '*.html'
-  ], {
-    base: '.'
-  })
-    .pipe(gulp.dest('build'));
-});
-
-gulp.task('sprite', function () {
-  var spriteData = gulp.src('img/*.png').pipe(spritesmith({
-    imgName: 'sprite.png',
-    cssName: 'sprite.css'
-  }));
-  var imgStream = spriteData.img
-    .pipe(gulp.dest('img/'));
-  var cssStream = spriteData.css
-    .pipe(gulp.dest('sass/global/'));
-  return gulpMerge(imgStream, cssStream);
-});
-
-gulp.task('copyBootstrapJS', function(){
-  return gulp.src(['node_modules/bootstrap-sass/assets/javascripts/*.js'])
-    .pipe(gulp.dest('build/js'));
-});
-
 gulp.task('copyJS', function(){
   return gulp.src(['js/*.js'])
     .pipe(gulp.dest('build/js'));
@@ -91,7 +56,17 @@ gulp.task('copyHtml', function(){
   return gulp.src(['*.html'], {base: '.'})
     .pipe(gulp.dest('build'));
 });
-
+gulp.task('copy', function(){
+  return gulp.src([
+    'fonts/**/*.{eot,svg,ttf,woff,woff2}',
+    'js/**',
+    'img/*.svg',
+    '*.html'
+  ], {
+    base: '.'
+  })
+    .pipe(gulp.dest('build'));
+});
 gulp.task('images', function() {
   return gulp.src('img/**/*.{jpg,png,gif}')
     .pipe(imagemin([
@@ -101,14 +76,29 @@ gulp.task('images', function() {
     .pipe(gulp.dest('build/img'));
 });
 
-gulp.task('build', function(fn){
-  run('clean', 'copy', 'copyBootstrapJS', 'images', 'style', fn);
+gulp.task('copyBootstrapJS', function(){
+  return gulp.src(['node_modules/bootstrap-sass/assets/javascripts/*.js'])
+    .pipe(gulp.dest('build/js'));
 });
 
-gulp.task('server', function(){
-  run('build', 'serve');
+gulp.task('browser-sync', function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
 });
-gulp.task('deploy', function() {
-  return gulp.src('build/**/*')
-    .pipe(ghPages());
+
+gulp.task('watch', function() {
+    gulp.watch('sass/**/*.{scss,sass}', gulp.parallel('style')); // Наблюдение за sass файлами
+    gulp.watch('*.html', gulp.parallel('copyHtml'));
+    gulp.watch('js/*.js', gulp.parallel('copyJS'));
+    gulp.watch('build/*.html').on('change', server.reload);
+    gulp.watch('build/js/*.js').on('change', server.reload);
 });
+gulp.task('build', gulp.parallel('clean', 'copy', 'copyBootstrapJS', 'images', 'style'));
+
+gulp.task('server', gulp.parallel('build', 'serve', 'watch'));
+
+gulp.task('default', gulp.parallel('style', 'serve', 'watch'));
